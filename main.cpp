a9c6be83c5a8a1c5175b7af088f3745957827233
yusakul <32380039+yusakul@users.noreply.github.com>
2019-01-17 11:03:55 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "MD5.h"
+#include <windows.h>
+
+using namespace md5;
+
+int main()
+{
+	MD5 md5;
+	unsigned char w[] = "some memory data";
+	// 计算一段内存数据的md5
+	puts(md5.digestMemory(w, sizeof(w)));
+
+	// 计算string的md5
+	char str[] = "hello";
+	printf("%s\n",md5.digestString(str));
+
+	// print the digest for a binary file on disk.
+	char path[] = "C:\\WINDOWS\\notepad.exe";
+	puts(md5.digestFile(path));
+
+	system("pause");
+	return 0;
+}
